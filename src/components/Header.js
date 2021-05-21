78907f436d4b2a80a9b0e406883eebf7541a788c
daikiku10 <soccerdk19.910@gmail.com>
2021-05-22 02:57:41 +0900

    header装飾

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Container } from '@material-ui/core';
+import { AppBar, Container, Typography } from '@material-ui/core';
 
 
 const Header = () => {
   return(
-    <Container maxWidth="sm">
-      <div>ToDoリスト</div>
-    </Container>
+    <AppBar position="static">
+      <Typography variant="h6">
+        ToDoリスト
+      </Typography>
+    </AppBar>
   )
 }
 
